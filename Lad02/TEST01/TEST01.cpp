487d6ef4acf6919fe77567e3fabeb46d056e83dd
JIrsaskMUT <6812100006@mut.ac.th>
2025-11-23 17:30:35 +0700

    111

---
+++ Lad02/TEST01/TEST01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    std::cout << "Hello Led02\n";
+
+    // Display additional information
+    std::cout << "Name: Sutit Ongart\n"
+        << "Course: MIIA0106\n"
+        << "Email: sutit.o@maori-softech.com\n";
+
+    return 0;
+}
---
+++ Lad02/Test02/Test02.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Name: Sutit Ongart\n"
+        << "Course: MIIA0106\n"
+        << "Email: sutit.o@maori-softech.com\n";
+    return 0;
+}
